0c3eede8bf81b7917c13c5b31dd1543d020ff2a9
Tayfun-senturk <tayfunsenturk8@gmail.com>
2025-12-28 18:14:02 +0300

    Disable Tomcat shutdown port to prevent noisy logs during PaaS health checks

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -eu
 
+# Disable the Tomcat shutdown port to avoid noisy "Invalid shutdown command" logs
+# when PaaS health checks or probes hit non-HTTP ports.
+sed -i 's/<Server port="8005"/<Server port="-1"/' "$CATALINA_HOME/conf/server.xml"
+
 if [ -n "${PORT:-}" ]; then
   # Many PaaS providers inject $PORT; update Tomcat connector port.
   sed -i "s/port=\"8080\"/port=\"${PORT}\"/" "$CATALINA_HOME/conf/server.xml"
 fi
 
 exec catalina.sh run
-
